d87efe653a2e7562f0c96925609c7962f31383d2
unknown <thiagobalves13@gmail.com>
2021-06-24 12:29:25 -0300

    feat(aula02)[spring_data]:Conclusao_da_aula_02

--- src/main/java/br/com/alura/spring/data/orm/UnidadeTrabalho.java
+++ src/main/java/br/com/alura/spring/data/orm/UnidadeTrabalho.java
@@ -3,29 +3,24 @@ package br.com.alura.spring.data.orm;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "cargos")
-public class Cargo {
-	
+@Table(name = "unidade_trabalho")
+public class UnidadeTrabalho {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String descricao;
-	@OneToMany
-	private List<Funcionario> funcionario;
-	
-	public List<Funcionario> getFuncionario() {
-		return funcionario;
-	}
-	public void setFuncionario(List<Funcionario> funcionario) {
-		this.funcionario = funcionario;
-	}
+	private String endereco;
+	@ManyToMany(mappedBy = "unidadeTrabalhos", fetch= FetchType.EAGER)
+	private List<Funcionario> funcionarios;
 	public Integer getId() {
 		return id;
 	}
@@ -38,9 +33,16 @@ public class Cargo {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	@Override
-	public String toString() {
-		return "Cargo [id=" + id + ", descricao=" + descricao + "]";
+	public String getEndereco() {
+		return endereco;
 	}
-	
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
 }
